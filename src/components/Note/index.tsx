69ad7dd27fbda88158cbce571d3dd96040594584
Ecaterina Baba <e.baba@s7.ru>
2022-05-29 15:22:48 +0300

    Add GET, POST requests, render data

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import { useAppSelector } from "../../app/hooks";
 import { selectAuth, LoginStatus } from "../Login/authslice";
+import axios from 'axios';
+
+export interface AuthProps {
+  apiToken: string;
+  getNote: any;
+  createNote: any
+}
+
+export interface Error {
+  title: string;
+  info: string;
+}
 
 export function Note() {
   const auth = useAppSelector(selectAuth);
+  const [error, setError] = useState<Error>({title: '', info: ''});
 
   if (auth.status !== LoginStatus.LOGGED_IN) return null;
   const {
@@ -11,13 +24,65 @@ export function Note() {
     user: { id: userId },
   } = auth;
 
+  const api = axios.create({
+    baseURL: 'https://60b793ec17d1dc0017b8a6bc.mockapi.io/',
+    headers: {
+      'Authorization': `Bearer ${apiToken}`,
+    },
+  });
+
+  const getNote = async () => {
+    try {
+      const {data} = await api.get(`/users/${userId}/`);
+      setError({title: '', info: ''});
+      return data.note;
+    } catch (e) {
+      setError({title: 'Something went wrong...', info: e.message});
+    }
+  }
+
+  const createNote = async (note: string) => {
+    try {
+      await api.put(`/users/${userId}/`, {note});
+      setError({title: '', info: ''});
+    } catch (e) {
+      setError({title: 'Something went wrong...', info: e.message});
+    }
+  }
+
   return (
     <div>
-      <NoteField />
+      <NoteField apiToken={apiToken} getNote={getNote} createNote={createNote} />
+      {
+         error.title &&
+          (<span className={'error-message'}>
+            <p>{error.title}</p>
+            <p>{error.info}</p>
+          </span>)
+      }
     </div>
   );
 }
 
-function NoteField() {
-  return <textarea defaultValue="Note goes here..."></textarea>;
+function NoteField({ apiToken, getNote, createNote }: AuthProps) {
+  const [note, setNote] = useState<string>('');
+
+  useEffect(() => {
+    getNote().then((response: string) => setNote(response));
+  }, [apiToken]);
+
+  const onChangeNote = async (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const {target: {value}} = event;
+
+    setNote(value);
+    await createNote(value);
+  }
+
+  return(
+      <textarea
+        placeholder="Note goes here..."
+        value={note}
+        onChange={onChangeNote}
+  />
+  );
 }
